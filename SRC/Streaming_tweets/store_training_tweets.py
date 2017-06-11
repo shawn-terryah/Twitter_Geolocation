40eb885145c4b0f44407815c3505ee52cf84817b
Shawn Terryah <shawn.terryah@gmail.com>
2017-06-11 14:54:37 -0500

    Update store_training_tweets.py

--- SRC/Streaming_tweets/store_training_tweets.py
+++ SRC/Streaming_tweets/store_training_tweets.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     consumer_key = "your_consumer_key"
     consumer_secret = "your_consumer_secret"
     access_token = "your_access_token"
-    access_token_secret = 	"your_access_token_secret"
+    access_token_secret = "your_access_token_secret"
 
     auth1 = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth1.set_access_token(access_token, access_token_secret)
