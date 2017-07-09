6c200d40b43ce47e24083b295e2b9c56170a629b
Shawn Terryah <shawn.terryah@gmail.com>
2017-07-09 11:21:41 -0500

    Create tweets.py

---
+++ SRC/Training_Model/tweets.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import nltk
+from nltk.tokenize import TweetTokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.externals import joblib
+
