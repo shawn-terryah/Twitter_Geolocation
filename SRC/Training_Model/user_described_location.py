3f095c5d626c91d515c179abfcdab6160bf2d97e
Shawn Terryah <shawn.terryah@gmail.com>
2017-07-09 10:56:50 -0500

    Update user_described_location.py

--- SRC/Training_Model/user_described_location.py
+++ SRC/Training_Model/user_described_location.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+
+base_location_doc = base_df['user_described_location'].values
+base_location_Y = base_df['closest_major_city'].values
+
+meta_location_doc = meta_df['user_described_location'].values
+meta_location_Y = meta_df['closest_major_city'].values
