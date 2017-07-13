327eed9dc9bfe4d0a201cb3629b424f631d21ea4
Shawn Terryah <shawn.terryah@gmail.com>
2017-07-13 16:39:58 -0500

    Update store_training_tweets.py

--- SRC/Streaming_tweets/store_training_tweets.py
+++ SRC/Streaming_tweets/store_training_tweets.py
@@ -3,9 +3,8 @@ import json
 from pymongo import MongoClient
 
 class StreamListener(tweepy.StreamListener):
-    """
-    tweepy.StreamListener is a class provided by tweepy used to access
-    the Twitter Streaming API. It allows us to retrieve tweets in real time.
+    """tweepy.StreamListener is a class provided by tweepy used to access
+    the Twitter Streaming API to collect tweets in real-time.
     """
 
     def on_connect(self):
@@ -21,23 +20,23 @@ class StreamListener(tweepy.StreamListener):
         """This will be called each time we receive stream data"""
         client = MongoClient()
 
-        # Store the tweet data in a database called 'training_tweets' in MongoDB, if 
-        # 'training_tweets' does not already exist it will be created for you
+        # I stored the tweet data in a database called 'training_tweets' in MongoDB, if 
+        # 'training_tweets' does not exist it will be created for you.
         db = client.training_tweets
 
         # Decode JSON
         datajson = json.loads(data)
 
-        # I'm only storing tweets in English
+        # I'm only storing tweets in English. I stored the data for these tweets in a collection 
+        # called 'training_tweets_collection' of the 'training_tweets' database. If 
+        # 'training_tweets_collection' does not exist it will be created for you. 
         if "lang" in datajson and datajson["lang"] == "en":
-            # Store tweet data in the 'training_tweets_collection' collection of the 'training_tweets' database, 
-            # if the 'training_tweets_collection' does not already exist it will be created for you
             db.training_tweets_collection.insert_one(datajson)
 
 
 if __name__ == "__main__":
 
-    # these are provided to you through the Twitter API after you create a account
+    # These are provided to you through the Twitter API after you create a account
     consumer_key = "your_consumer_key"
     consumer_secret = "your_consumer_secret"
     access_token = "your_access_token"
@@ -46,13 +45,12 @@ if __name__ == "__main__":
     auth1 = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth1.set_access_token(access_token, access_token_secret)
 
-    '''LOCATIONS are the coordinate (long , lat) corners for a box that restricts the geographic area 
-    from which you will receive tweets. The first two define the southwest corner of the box
-    and the second two define the northeast corner of the box. Below are the coordinates for 
-    three boxes that define the contigous US, Alaska, and Hawaii'''
-    LOCATIONS = [-124.7771694, 24.520833, -66.947028, 49.384472,
-                 -164.639405, 58.806859, -144.152365, 71.76871,
-                 -160.161542, 18.776344, -154.641396, 22.878623]
+    # LOCATIONS are the longitude, latitude coordinate corners for a box that restricts the 
+    # geographic area from which you will stream tweets. The first two define the southwest
+    # corner of the box and the second two define the northeast corner of the box. 
+    LOCATIONS = [-124.7771694, 24.520833, -66.947028, 49.384472,        # Contiguous US
+                 -164.639405, 58.806859, -144.152365, 71.76871,         # Alaska
+                 -160.161542, 18.776344, -154.641396, 22.878623]        # Hawaii
 
     stream_listener = StreamListener(api=tweepy.API(wait_on_rate_limit=True))
     stream = tweepy.Stream(auth=auth1, listener=stream_listener)
